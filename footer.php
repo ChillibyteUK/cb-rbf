cb4d4f8d0a59279a8f54f6dae3713f979bd4ddef
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-08-10 15:11:18 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -135,7 +135,7 @@ if (get_field('gtm_property', 'options')) {
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="donateModalLabel">Donate</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
