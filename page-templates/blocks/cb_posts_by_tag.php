7689725e8c35148f7857962a3cc52a817453ce7a
Matthew Pont <matthew.pont@gmail.com>
2024-02-08 10:41:45 +0000

    Update cb_posts_by_tag.php
    Editing this block to enable you to change the style of the blocks and also hide the read more link.

--- page-templates/blocks/cb_posts_by_tag.php
+++ page-templates/blocks/cb_posts_by_tag.php
@@ -21,9 +21,13 @@ $title = get_field('title') ?: 'Related News &amp; Events';
                 $cats = get_the_category(get_the_ID());
                 $cat = $cats[0]->slug;
                 ?>
+
+<?php
+if( get_field('show_excerpt') ) {
+?>
                 <div class="slider__item px-3">
                     <a href="<?=get_the_permalink(get_the_ID())?>">
-                        <div class="card card--<?=$cat?>">
+                        <div class="card card--<?=$cat?> card--<?=$theme?>">
                             <div class="news__image_container">
                                 <div class="news__image" style="background-image:url('<?=get_the_post_thumbnail_url(get_the_ID(),'large')?>')"></div>
                             </div>
@@ -38,11 +42,36 @@ $title = get_field('title') ?: 'Related News &amp; Events';
                         </div>
                     </a>
                 </div>
+<?php
+} else {
+?>
+                <?php
+                $img = get_the_post_thumbnail_url(get_the_ID(),'large');
+                ?>
+                <div class="slider__item px-3">
+                    <a href="<?=get_the_permalink()?>">
+                        <div class="card card--<?=$theme?>">
+                            <div class="card__title"><?=get_the_title()?></div>
+                            <div class="card__image" style="background-image:url(<?=$img?>)"></div>
+                            <div class="card__link">Read more</div>
+                        </div>
+                    </a>
+                </div>
+<?php
+}
+?>
+
                 <?php
             }
             ?>
         </div>
+<?php
+if( get_field('show_read_more') ) {
+?>
         <div class="text-center"><a href="/news/" class="btn btn-default">Read more</a></div>
+<?php
+}
+?>
     </div>
 </section>
 <?php
